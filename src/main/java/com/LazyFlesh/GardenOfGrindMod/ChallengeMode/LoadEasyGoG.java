dd1ced99de22091904067c46386bc9bda8ce114b
LazyFlesh <40587985+LazyFlesh@users.noreply.github.com>
2026-06-27 14:43:06 -0700

    easy gog chances...done?

--- src/main/java/com/LazyFlesh/GardenOfGrindMod/ChallengeMode/LoadEasyGoG.java
+++ src/main/java/com/LazyFlesh/GardenOfGrindMod/ChallengeMode/LoadEasyGoG.java
@@ -1,21 +1,27 @@
 package com.LazyFlesh.GardenOfGrindMod.ChallengeMode;
 
+import static com.dreammaster.scripts.IScriptLoader.missing;
 import static gregtech.api.enums.Mods.AppliedEnergistics2;
-import static gregtech.api.recipe.RecipeMaps.centrifugeRecipes;
+import static gregtech.api.enums.Mods.IndustrialCraft2;
+import static gregtech.api.recipe.RecipeMaps.assemblerRecipes;
 import static gregtech.api.util.GTModHandler.getModItem;
-import static gregtech.api.util.GTRecipeBuilder.SECONDS;
 import static gregtech.api.util.GTRecipeBuilder.TICKS;
-import static gregtech.api.util.GTRecipeBuilder.WILDCARD;
 import static gtPlusPlus.api.recipe.GTPPRecipeMaps.multiblockRockBreakerRecipes;
 
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.fluids.FluidRegistry;
+import net.minecraftforge.fluids.FluidStack;
 
 import com.LazyFlesh.GardenOfGrindMod.GardenOfGrindMod;
 import com.LazyFlesh.GardenOfGrindMod.loaders.GoGItemList;
+import com.dreammaster.main.MainRegistry;
 import com.dreammaster.recipes.ShapedUniversalRecipe;
+import com.dreammaster.recipes.ShapelessUniversalRecipe;
+import com.dreammaster.scripts.IngredientFactory;
+import com.gtnewhorizon.cropsnh.api.CropsNHItemList;
 
 import cpw.mods.fml.common.registry.GameRegistry;
 import gregtech.api.enums.GTValues;
@@ -34,61 +40,221 @@ public class LoadEasyGoG extends ModeLoader {
     }
 
     public static void registerRecipes() {
-        GameRegistry.addRecipe(
-            new ShapedUniversalRecipe(
-                GoGItemList.ThermiteBucket.get(1),
-                "abc",
-                "abc",
-                " e ",
-                'a',
-                GTOreDictUnificator.get(OrePrefixes.dust, Materials.Magnetite, 1),
-                'b',
-                GTOreDictUnificator.get(OrePrefixes.dust, Materials.Obsidian, 1),
-                'c',
-                GTOreDictUnificator.get(OrePrefixes.dust, Materials.Zinc, 1),
-                'e',
-                new ItemStack(Items.bucket, 1)));
+        try {
+            GameRegistry.addRecipe(
+                new ShapelessUniversalRecipe(
+                    CropsNHItemList.cropSticks.get(1),
+                    new ItemStack(Items.bone, 1),
+                    new ItemStack(Items.bone, 1),
+                    new ItemStack(Items.bone, 1),
+                    new ItemStack(Items.bone, 1)));
 
-        GameRegistry.addRecipe(
-            new ShapedUniversalRecipe(
-                GoGItemList.GravelDust.get(1),
-                "a  ",
-                "b  ",
-                "   ",
-                'a',
-                "craftingToolHardHammer",
-                'b',
-                new ItemStack(Item.getItemFromBlock(Blocks.gravel), 1)));
+            GameRegistry.addRecipe(
+                new ShapelessUniversalRecipe(
+                    CropsNHItemList.cropSticks.get(1),
+                    new ItemStack(Items.arrow, 1),
+                    new ItemStack(Items.arrow, 1),
+                    new ItemStack(Items.arrow, 1),
+                    new ItemStack(Items.arrow, 1)));
+
+            GameRegistry.addRecipe(
+                new ShapedUniversalRecipe(
+                    GoGItemList.ThermiteBucket.get(1),
+                    "abc",
+                    "abc",
+                    " e ",
+                    'a',
+                    GTOreDictUnificator.get(OrePrefixes.dust, Materials.Magnetite, 1),
+                    'b',
+                    GTOreDictUnificator.get(OrePrefixes.dust, Materials.Obsidian, 1),
+                    'c',
+                    GTOreDictUnificator.get(OrePrefixes.dust, Materials.Zinc, 1),
+                    'e',
+                    new ItemStack(Items.bucket, 1)));
+
+            GameRegistry.addRecipe(
+                new ShapedUniversalRecipe(
+                    GoGItemList.GravelDust.get(1),
+                    "a  ",
+                    "b  ",
+                    "   ",
+                    'a',
+                    "craftingToolHardHammer",
+                    'b',
+                    new ItemStack(Item.getItemFromBlock(Blocks.gravel), 1)));
+        } catch (Exception e) {
+            MainRegistry.LOGGER.error("A Garden of Grind Skyblock recipe went wrong:");
+            e.printStackTrace();
+        }
 
         GTValues.RA.stdBuilder()
-            .itemInputs(new ItemStack(Item.getItemFromBlock(Blocks.gravel), 1))
-            .itemOutputs(
-                GTOreDictUnificator.get(OrePrefixes.crushed, Materials.Magnetite, 1),
-                GTOreDictUnificator.get(OrePrefixes.crushed, Materials.Tetrahedrite, 1),
-                GTOreDictUnificator.get(OrePrefixes.dustImpure, Materials.CassiteriteSand, 1),
-                GTOreDictUnificator.get(OrePrefixes.crushed, Materials.Redstone, 1),
-                GTOreDictUnificator.get(OrePrefixes.crushed, Materials.Zinc, 1),
-                GTOreDictUnificator.get(OrePrefixes.dustImpure, Materials.Silver, 1))
-            .outputChances(50_00, 40_00, 25_00, 15_00, 10_00, 10_00)
             .circuit(1)
-            .duration(1 * SECONDS)
+            .fluidInputs(new FluidStack(FluidRegistry.getFluid("xpjuice"), 1000))
+            .itemOutputs(getModItem(Mods.Minecraft.ID, "coal", 1, 1, missing))
+            .duration(16 * TICKS)
             .eut(TierEU.RECIPE_ULV)
-            .addTo(centrifugeRecipes);
-
+            .addTo(assemblerRecipes);
         GTValues.RA.stdBuilder()
-            .itemInputs(new ItemStack(Item.getItemFromBlock(Blocks.sand), 1, WILDCARD))
-            .itemOutputs(
-                GTOreDictUnificator.get(OrePrefixes.dustImpure, Materials.CassiteriteSand, 1),
-                GTOreDictUnificator.get(OrePrefixes.dust, Materials.Sulfur, 1),
-                GTOreDictUnificator.get(OrePrefixes.dust, Materials.GarnetSand, 1),
-                GTOreDictUnificator.get(OrePrefixes.dust, Materials.Silver, 1),
-                GTOreDictUnificator.get(OrePrefixes.dustImpure, Materials.Asbestos, 1),
-                GTOreDictUnificator.get(OrePrefixes.dustImpure, Materials.Mica, 1))
-            .outputChances(50_00, 40_00, 25_00, 15_00, 10_00, 10_00)
-            .circuit(1)
-            .duration(1 * SECONDS)
+            .circuit(2)
+            .fluidInputs(new FluidStack(FluidRegistry.getFluid("xpjuice"), 1000))
+            .itemOutputs(getModItem(Mods.Minecraft.ID, "redstone", 1, 1, missing))
+            .duration(16 * TICKS)
             .eut(TierEU.RECIPE_ULV)
-            .addTo(centrifugeRecipes);
+            .addTo(assemblerRecipes);
+        GTValues.RA.stdBuilder()
+            .circuit(3)
+            .fluidInputs(new FluidStack(FluidRegistry.getFluid("xpjuice"), 1000))
+            .itemOutputs(GTOreDictUnificator.get(OrePrefixes.dust, Materials.Glass, 1L))
+            .duration(16 * TICKS)
+            .eut(TierEU.RECIPE_ULV)
+            .addTo(assemblerRecipes);
+        GTValues.RA.stdBuilder()
+            .circuit(4)
+            .fluidInputs(new FluidStack(FluidRegistry.getFluid("xpjuice"), 1000))
+            .itemOutputs(getModItem(Mods.Minecraft.ID, "string", 1, 1, missing))
+            .duration(16 * TICKS)
+            .eut(TierEU.RECIPE_ULV)
+            .addTo(assemblerRecipes);
+        GTValues.RA.stdBuilder()
+            .circuit(5)
+            .fluidInputs(new FluidStack(FluidRegistry.getFluid("xpjuice"), 1000))
+            .itemOutputs(GTOreDictUnificator.get(OrePrefixes.dust, Materials.Clay, 1L))
+            .duration(16 * TICKS)
+            .eut(TierEU.RECIPE_ULV)
+            .addTo(assemblerRecipes);
+        GTValues.RA.stdBuilder()
+            .circuit(6)
+            .fluidInputs(new FluidStack(FluidRegistry.getFluid("xpjuice"), 1000))
+            .itemOutputs(GTOreDictUnificator.get(OrePrefixes.dust, Materials.Flint, 1L))
+            .duration(16 * TICKS)
+            .eut(TierEU.RECIPE_ULV)
+            .addTo(assemblerRecipes);
+        GTValues.RA.stdBuilder()
+            .circuit(7)
+            .fluidInputs(new FluidStack(FluidRegistry.getFluid("xpjuice"), 2000))
+            .itemOutputs(GTOreDictUnificator.get(OrePrefixes.dust, Materials.Gypsum, 1L))
+            .duration(16 * TICKS)
+            .eut(TierEU.RECIPE_ULV)
+            .addTo(assemblerRecipes);
+        GTValues.RA.stdBuilder()
+            .circuit(8)
+            .fluidInputs(new FluidStack(FluidRegistry.getFluid("xpjuice"), 2000))
+            .itemOutputs(GTOreDictUnificator.get(OrePrefixes.dust, Materials.Calcite, 1L))
+            .duration(16 * TICKS)
+            .eut(TierEU.RECIPE_ULV)
+            .addTo(assemblerRecipes);
+        GTValues.RA.stdBuilder()
+            .circuit(9)
+            .fluidInputs(new FluidStack(FluidRegistry.getFluid("xpjuice"), 2000))
+            .itemOutputs(GTOreDictUnificator.get(OrePrefixes.dust, Materials.Sulfur, 1L))
+            .duration(16 * TICKS)
+            .eut(TierEU.RECIPE_ULV)
+            .addTo(assemblerRecipes);
+        GTValues.RA.stdBuilder()
+            .circuit(10)
+            .fluidInputs(new FluidStack(FluidRegistry.getFluid("xpjuice"), 2000))
+            .itemOutputs(GTOreDictUnificator.get(OrePrefixes.dust, Materials.Iron, 1L))
+            .duration(16 * TICKS)
+            .eut(TierEU.RECIPE_ULV)
+            .addTo(assemblerRecipes);
+        GTValues.RA.stdBuilder()
+            .circuit(11)
+            .fluidInputs(new FluidStack(FluidRegistry.getFluid("xpjuice"), 3000))
+            .itemOutputs(GTOreDictUnificator.get(OrePrefixes.dust, Materials.Tin, 1L))
+            .duration(16 * TICKS)
+            .eut(TierEU.RECIPE_ULV)
+            .addTo(assemblerRecipes);
+        GTValues.RA.stdBuilder()
+            .circuit(12)
+            .fluidInputs(new FluidStack(FluidRegistry.getFluid("xpjuice"), 3000))
+            .itemOutputs(GTOreDictUnificator.get(OrePrefixes.dust, Materials.Copper, 1L))
+            .duration(16 * TICKS)
+            .eut(TierEU.RECIPE_ULV)
+            .addTo(assemblerRecipes);
+        GTValues.RA.stdBuilder()
+            .circuit(13)
+            .fluidInputs(new FluidStack(FluidRegistry.getFluid("xpjuice"), 3000))
+            .itemOutputs(GTOreDictUnificator.get(OrePrefixes.dust, Materials.Nickel, 1L))
+            .duration(16 * TICKS)
+            .eut(TierEU.RECIPE_ULV)
+            .addTo(assemblerRecipes);
+        GTValues.RA.stdBuilder()
+            .circuit(14)
+            .fluidInputs(new FluidStack(FluidRegistry.getFluid("xpjuice"), 3000))
+            .itemOutputs(GTOreDictUnificator.get(OrePrefixes.dust, Materials.RubberRaw, 1L))
+            .duration(16 * TICKS)
+            .eut(TierEU.RECIPE_ULV)
+            .addTo(assemblerRecipes);
+        GTValues.RA.stdBuilder()
+            .circuit(15)
+            .fluidInputs(new FluidStack(FluidRegistry.getFluid("xpjuice"), 4000))
+            .itemOutputs(GTOreDictUnificator.get(OrePrefixes.dust, Materials.Obsidian, 1L))
+            .duration(16 * TICKS)
+            .eut(TierEU.RECIPE_ULV)
+            .addTo(assemblerRecipes);
+        GTValues.RA.stdBuilder()
+            .circuit(16)
+            .fluidInputs(new FluidStack(FluidRegistry.getFluid("xpjuice"), 4000))
+            .itemOutputs(GTOreDictUnificator.get(OrePrefixes.dust, Materials.Brick, 1L))
+            .duration(16 * TICKS)
+            .eut(TierEU.RECIPE_ULV)
+            .addTo(assemblerRecipes);
+        GTValues.RA.stdBuilder()
+            .circuit(17)
+            .fluidInputs(new FluidStack(FluidRegistry.getFluid("xpjuice"), 5000))
+            .itemOutputs(GTOreDictUnificator.get(OrePrefixes.dust, Materials.CastIron, 1L))
+            .duration(16 * TICKS)
+            .eut(TierEU.RECIPE_ULV)
+            .addTo(assemblerRecipes);
+        GTValues.RA.stdBuilder()
+            .circuit(18)
+            .fluidInputs(new FluidStack(FluidRegistry.getFluid("xpjuice"), 7000))
+            .itemOutputs(IngredientFactory.getModItem(IndustrialCraft2.ID, "itemHarz", 1, 0))
+            .duration(16 * TICKS)
+            .eut(TierEU.RECIPE_ULV)
+            .addTo(assemblerRecipes);
+        GTValues.RA.stdBuilder()
+            .circuit(19)
+            .fluidInputs(new FluidStack(FluidRegistry.getFluid("xpjuice"), 7000))
+            .itemOutputs(GTOreDictUnificator.get(OrePrefixes.dustTiny, Materials.Arsenic, 1L))
+            .duration(16 * TICKS)
+            .eut(TierEU.RECIPE_ULV)
+            .addTo(assemblerRecipes);
+        GTValues.RA.stdBuilder()
+            .circuit(20)
+            .fluidInputs(new FluidStack(FluidRegistry.getFluid("xpjuice"), 8000))
+            .itemOutputs(GTOreDictUnificator.get(OrePrefixes.dust, Materials.Zinc, 1L))
+            .duration(16 * TICKS)
+            .eut(TierEU.RECIPE_ULV)
+            .addTo(assemblerRecipes);
+        GTValues.RA.stdBuilder()
+            .circuit(21)
+            .fluidInputs(new FluidStack(FluidRegistry.getFluid("xpjuice"), 8000))
+            .itemOutputs(GTOreDictUnificator.get(OrePrefixes.dustSmall, Materials.Silver, 1L))
+            .duration(16 * TICKS)
+            .eut(TierEU.RECIPE_ULV)
+            .addTo(assemblerRecipes);
+        GTValues.RA.stdBuilder()
+            .circuit(22)
+            .fluidInputs(new FluidStack(FluidRegistry.getFluid("xpjuice"), 8000))
+            .itemOutputs(GTOreDictUnificator.get(OrePrefixes.dust, Materials.Gold, 1L))
+            .duration(16 * TICKS)
+            .eut(TierEU.RECIPE_ULV)
+            .addTo(assemblerRecipes);
+        GTValues.RA.stdBuilder()
+            .circuit(23)
+            .fluidInputs(new FluidStack(FluidRegistry.getFluid("xpjuice"), 8000))
+            .itemOutputs(GTOreDictUnificator.get(OrePrefixes.dustTiny, Materials.Gallium, 1L))
+            .duration(16 * TICKS)
+            .eut(TierEU.RECIPE_ULV)
+            .addTo(assemblerRecipes);
+        GTValues.RA.stdBuilder()
+            .circuit(24)
+            .fluidInputs(new FluidStack(FluidRegistry.getFluid("xpjuice"), 9000))
+            .itemOutputs(GTOreDictUnificator.get(OrePrefixes.dustSmall, Materials.CobaltBrass, 1L))
+            .duration(16 * TICKS)
+            .eut(TierEU.RECIPE_ULV)
+            .addTo(assemblerRecipes);
 
         if (Mods.AppliedEnergistics2.isModLoaded()) {
             GTValues.RA.stdBuilder()
